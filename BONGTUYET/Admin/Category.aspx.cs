076359fbddb127662c736d36505a8263a050cc14
DuongVo01 <votrungduong1@gmail.com>
2024-04-13 22:18:59 +0700

    Add project files.

---
+++ BONGTUYET/Admin/Category.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace BONGTUYET.Admin
+{
+    public partial class Category : System.Web.UI.Page
+    {
+        SqlConnection con;
+        SqlCommand cmd;
+        SqlDataAdapter sad;
+        DataTable dt;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnAddOrUpdate_Click(object sender, EventArgs e)
+        {
+            string actionName = string.Empty, imagePath = string.Empty, fileExtension = string.Empty;
+            bool isValidToExecute = false;
+            int categoryId = Convert.ToInt32(hfCategoryId.Value);
+            con = new SqlConnection(Utils.getConnection());
+            cmd = new SqlCommand("Category_Crud", con);
+            cmd.Parameters.AddWithValue("@Action", categoryId == 0 ? "INSERT" :  "UPDATE");
+            cmd.Parameters.AddWithValue("@CategoryId", categoryId);
+            cmd.Parameters.AddWithValue("@CategoryName", txtCategoryName.Text.Trim());
+            cmd.Parameters.AddWithValue("@IsActive", cbIsActive.Checked);
+            if (fuCategoryImage.HasFile)
+            {
+
+            }
+        }
+
+        protected void btnClear_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ BONGTUYET/Utils.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+
+namespace BONGTUYET
+{
+    public class Utils
+    {
+        SqlConnection con;
+        SqlCommand cmd;
+        SqlDataAdapter sad;
+        SqlDataReader sdr;
+        DataTable dt;
+
+        public static string getConnection()
+        {
+            return ConfigurationManager.ConnectionStrings["cs"].ConnectionString;
+        }
+    }
+}
